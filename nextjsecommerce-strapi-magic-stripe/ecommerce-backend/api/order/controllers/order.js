6d1e1a42d706fe3fc81ac22a529704ff230870e1
nooobcoder <suryashi2013@gmail.com>
2021-07-06 19:52:31 +0530

    changed default controller of strapi to make authenticated requests

--- nextjsecommerce-strapi-magic-stripe/ecommerce-backend/api/order/controllers/order.js
+++ nextjsecommerce-strapi-magic-stripe/ecommerce-backend/api/order/controllers/order.js
@@ -1,8 +1,29 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    const { user } = ctx.state; // This is the user issued my magic
+
+    let entities;
+    if (ctx.query._q)
+      entities = await strapi.services.order.search({
+        ...ctx.query,
+        user: user.id,
+      });
+    else
+      entities = await strapi.services.order.find({
+        ...ctx.query,
+        user: user.id,
+      });
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.order })
+    );
+  },
+};
