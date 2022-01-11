04ca1283dca0a73d61a5de187fb7ea4fa5c1a22c
Ankur Paul <suryashi2013@gmail.com>
2022-01-11 23:20:29 +0530

    Completed Project

--- supabase-auth0/pages/api/auth/[...auth0].js
+++ supabase-auth0/pages/api/auth/[...auth0].js
@@ -1,3 +1,25 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleCallback } from "@auth0/nextjs-auth0";
 
-export default handleAuth();
+const afterCallback = async (req, res, session) => {
+	const payload = {
+		userId: session.user.sub,
+		exp: Math.floor(Date.now() / 1000) + 60 * 60,
+	};
+
+	session.user.accessToken = jwt.sign(
+		payload,
+		process.env.SUPABASE_SIGNING_SECRET
+	);
+
+	return session;
+};
+
+export default handleAuth({
+	async callback(req, res) {
+		try {
+			await handleCallback(req, res, { afterCallback });
+		} catch (error) {
+			res.status(error.status || 500).end(error.message);
+		}
+	},
+});
--- supabase-auth0/pages/index.js
+++ supabase-auth0/pages/index.js
@@ -1,7 +1,61 @@
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import { getSupabase } from "../utils/supabase";
 
-const Index = () => {
-	return <div className={styles.container}>Working!</div>;
+const Index = ({ user, todos }) => {
+	const [content, setContent] = useState("");
+	const [allTodos, setAllTodos] = useState([...todos]);
+
+	if (isLoading) return <div className={styles.container}>Loading...</div>;
+	if (error) return <div className={styles.container}>{error.message}</div>;
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const supabase = getSupabase(user.accessToken);
+		const resp = await supabase
+			.from("todo")
+			.insert({ content, user_id: user.sub });
+
+		setAllTodos([...todos, resp.data[0]]);
+		setContent("");
+	};
+
+	return (
+		<div className={styles.container}>
+			<p>
+				Welcome {user.name}!{" "}
+				<Link href="/api/auth/logout">
+					<a>Logout</a>
+				</Link>
+			</p>
+			<form onSubmit={handleSubmit}>
+				<input onChange={(e) => setContent(e.target.value)} value={content} />
+				<button>Add</button>
+			</form>
+			{allTodos?.length > 0 ? (
+				allTodos.map((todo) => <p key={todo.id}>{todo.content}</p>)
+			) : (
+				<p>You have completed all todos!</p>
+			)}
+		</div>
+	);
 };
+
+export const getServerSideProps = withPageAuthRequired({
+	async getServerSideProps({ req, res }) {
+		const {
+			user: { accessToken },
+		} = await getSession(req, res);
+
+		const supabase = getSupabase(accessToken);
+
+		const { data: todos } = await supabase.from("todo").select("*");
+
+		return {
+			props: { todos },
+		};
+	},
+});
 
 export default Index;
